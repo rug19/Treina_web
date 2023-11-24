dbfe900be03a787a021137b9a2ac660bb0cf5379
Ruan Gomes Costa <67665127+rug19@users.noreply.github.com>
2023-11-23 21:18:38 -0300

    Update Expressoes_regulares.js

--- JAVASCRIPT/Trabalhando_com_strings/Expressoes_regulares.js
+++ JAVASCRIPT/Trabalhando_com_strings/Expressoes_regulares.js
@@ -3,7 +3,7 @@
 /*Uma expressão regular é uma forma comum e concisa de representar algum tipo de padrão em texto. 
 Você pode usá-las, por exemplo, com o recurso de “Localizar e Substituir” para fazer alterações rapidamente em um grande volume de dados.*/
 
-//Verificando se possue a mesma palavra dentro de uma string
+//Verificando se possuem a mesma palavra dentro de uma string
 const resultado = /Ruan/g.test( `Ruan Gomes Costa`);
 
 console.log(resultado);
